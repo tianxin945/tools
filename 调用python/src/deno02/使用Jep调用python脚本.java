0ebff329f3eb58d7aad7e1e1b6128efc9eb2ab73
tianxin <935839646@qq.com>
2019-03-08 13:27:40 +0800

    线程共享的问题

---
+++ 调用python/src/deno02/使用Jep调用python脚本.java
@@ -0,0 +1,9 @@
+package deno02;
+
+/**
+ * @Author tianxin
+ * @Date 2019/3/8
+ * @Description:
+ */
+public class 使用Jep调用python脚本  {
+}
--- 调用python/src/test.java
+++ 调用python/src/test.java
@@ -11,13 +11,25 @@ import java.util.Date;
  */
 public class test {
     public static void main(String[] args) throws ParseException {
-        String time2 = "20100714";
+      /*  String time2 = "20100714";
         DateFormat df = new SimpleDateFormat("yyyyMMdd");
         Date date = df.parse(time2);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.DATE,-1);
         String format = df.format(calendar.getTime());
-        System.out.println(format);
+        System.out.println(format);*/
+      double danger = 5;
+      double total = 11;
+
+
+
+
+
+
+
+        String s =(String.valueOf((danger / (total>0?total:1))*100)+"0000").substring(0,5)+"%";
+
+        System.out.println(s);
     }
 }
