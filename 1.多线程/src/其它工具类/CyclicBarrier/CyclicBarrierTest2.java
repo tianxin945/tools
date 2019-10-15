f8b87c8f909a3182a3e3f21fbd49e05eb033b0f4
tianxin <935839646@qq.com>
2019-10-15 23:50:43 +0800

    代码更新

---
+++ 1.多线程/src/其它工具类/CyclicBarrier/CyclicBarrierTest2.java
@@ -0,0 +1,41 @@
+package 其它工具类.CyclicBarrier;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
+
+public class CyclicBarrierTest2 {
+
+    private static int SIZE = 5;
+    private static CyclicBarrier cb;
+
+    public static void main(String[] args) {
+
+        cb = new CyclicBarrier(SIZE, new Runnable() {
+            public void run() {
+                System.out.println("CyclicBarrier's parties is: " + Thread.currentThread().getName()+"----"+cb.getParties());
+            }
+        });
+
+        // 新建5个任务
+        for (int i = 0; i < SIZE; i++)
+            new InnerThread().start();
+    }
+
+    static class InnerThread extends Thread {
+        public void run() {
+            try {
+                System.out.println(Thread.currentThread().getName() + " wait for CyclicBarrier.");
+
+                // 将cb的参与者数量加1
+                cb.await();
+
+                // cb的参与者数量等于5时，才继续往后执行
+                System.out.println(Thread.currentThread().getName() + " continued.");
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
